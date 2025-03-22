dd25f888baa625c71627567066c9f89a77e77399
ayesha suleman <c2036445@newcastle.ac.uk>
2025-03-22 21:15:17 +0000

    project set up

---
+++ Ilm/Ilm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ilm
+//
+//  Created by Ayesha Suleman on 22/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.small)
+                .foregroundStyle(.tint)
+            Text("Hello, ayesha!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
