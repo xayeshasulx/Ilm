15d51370b1bd966f411669302be18e91ee3a3344
ayesha suleman <c2036445@newcastle.ac.uk>
2025-03-22 21:15:17 +0000

    project set up

---
+++ Ilm/Ilm/IlmApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlmApp.swift
+//  Ilm
+//
+//  Created by Ayesha Suleman on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IlmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ilm/IlmTests/IlmTests.swift
@@ -0,0 +1,17 @@
+//
+//  IlmTests.swift
+//  IlmTests
+//
+//  Created by Ayesha Suleman on 22/03/2025.
+//
+
+import Testing
+@testable import Ilm
+
+struct IlmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
